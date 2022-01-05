a9f3cbf45a59ea311daa99a5a937263a96af12e2
Arco Hsieh <yiyezhihen@gmail.com>
2022-01-05 11:25:51 +0800

    refactor: extract user_id from authorization bearer

--- pokemon_api/src/trader/get.rs
+++ pokemon_api/src/trader/get.rs
@@ -5,20 +5,23 @@ use pokemon_service::DOMAIN_REGISTRY;
 use super::query::TraderOrderLimitation;
 use super::res;
 use crate::api_error::ApiError;
+use crate::prelude::*;
 
-#[get("/api/traders/{trader_id}/orders")]
+#[get("/api/traders/orders")]
 pub async fn fetch_latest_orders_by_trader(
-  path: web::Path<i64>,
   query: web::Query<TraderOrderLimitation>,
+  user_id: UserId,
 ) -> Result<HttpResponse, ApiError> {
-  let trader_id = path.into_inner();
   let limit = query.limit;
+  let user_id = user_id.0;
 
   let domain_registry = DOMAIN_REGISTRY.get().unwrap().read().unwrap();
 
+  domain_registry.user_repository().fetch_by_id(user_id)?;
+
   let orders = domain_registry
     .order_query_commands()
-    .latest_orders_by_trader_id(trader_id, limit)?
+    .latest_orders_by_trader_id(user_id, limit)?
     .into_iter()
     .map(res::Order::from)
     .collect_vec();
